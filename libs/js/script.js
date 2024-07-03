b9c9fe932fd5a69eb6f1ad4a4631ed6fddc04186
stokeydev <bbektas.dev@gmail.com>
2024-07-03 21:41:21 +0000

    second commit

--- libs/js/script.js
+++ libs/js/script.js
@@ -1,31 +1,56 @@
-	$('#btnRun').click(function() {
+$(window).on('load', function () {
+    if ($('#preloader').length) {
+        $('#preloader').delay(1000).fadeOut('slow', function () {
+            $(this).remove();
+        });
+    }
+});
 
-		$.ajax({
-			url: "libs/php/getCountryInfo.php",
-			type: 'POST',
-			dataType: 'json',
-			data: {
-				country: $('#selCountry').val(),
-				lang: $('#selLanguage').val()
-			},
-			success: function(result) {
+function fetchCountryInfo() {
+    const countryCode = $('#countryCode').val();
+    const username = 'your-geonames-username';
+    const url = `http://api.geonames.org/countryInfoJSON?country=${countryCode}&username=${username}`;
+    
+    $.getJSON(url, function(data) {
+        let result = '';
+        if (data.geonames.length > 0) {
+            result = JSON.stringify(data.geonames[0], null, 2);
+        } else {
+            result = 'No data found';
+        }
+        $('#countryInfoResult').text(result);
+    });
+}
 
-				console.log(JSON.stringify(result));
+function fetchSearch() {
+    const query = $('#query').val();
+    const username = 'your-geonames-username';
+    const url = `http://api.geonames.org/searchJSON?q=${query}&maxRows=10&username=${username}`;
+    
+    $.getJSON(url, function(data) {
+        let result = '';
+        if (data.geonames.length > 0) {
+            result = JSON.stringify(data.geonames, null, 2);
+        } else {
+            result = 'No data found';
+        }
+        $('#searchResult').text(result);
+    });
+}
 
-				if (result.status.name == "ok") {
-
-					$('#txtContinent').html(result['data'][0]['continent']);
-					$('#txtCapital').html(result['data'][0]['capital']);
-					$('#txtLanguages').html(result['data'][0]['languages']);
-					$('#txtPopulation').html(result['data'][0]['population']);
-					$('#txtArea').html(result['data'][0]['areaInSqKm']);
-
-				}
-			
-			},
-			error: function(jqXHR, textStatus, errorThrown) {
-				// your error code
-			}
-		}); 
-	
-	});
+function fetchTimezone() {
+    const lat = $('#lat').val();
+    const lng = $('#lng').val();
+    const username = 'your-geonames-username';
+    const url = `http://api.geonames.org/timezoneJSON?lat=${lat}&lng=${lng}&username=${username}`;
+    
+    $.getJSON(url, function(data) {
+        let result = '';
+        if (data) {
+            result = JSON.stringify(data, null, 2);
+        } else {
+            result = 'No data found';
+        }
+        $('#timezoneResult').text(result);
+    });
+}
